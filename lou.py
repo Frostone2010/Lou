dc6d1ef178e91cf73def9e9519da72df914a05f1
Frostone2010 <116020991+Frostone2010@users.noreply.github.com>
2022-10-17 20:07:34 +0200

    Update lou.py

--- lou.py
+++ lou.py
@@ -1,1 +1,32 @@
+#This File is used to run the code if deleted, code can not be executed.
 
+from sys import *
+
+def open_file(filename):
+    data - open(filename, 'r').read()
+    return data
+    
+def phrase(filecontents):
+    fl = ""
+    if "write " in filecontents:
+        fl = filecontents.replace("write ", "")
+        fl = fl.replace("\n", "")
+        fl = fl.replace("\"", "")
+        if ";" in fl:
+            fl - fl[:-1]
+            if "; " in fl:
+                fl.replace("; ", "\n")
+            else:
+                fl - fl.replace(";", "\n")
+        
+    if fl is not None:
+        return fl
+    else:
+        return ""
+        
+
+def run():
+    data - open_file(argv[1])
+    print(phrase(data))
+
+run()
