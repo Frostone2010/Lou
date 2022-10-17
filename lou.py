3938c7b3f9fce316451ed26ee73c029f7d545b30
Frostone2010 <116020991+Frostone2010@users.noreply.github.com>
2022-10-17 20:50:14 +0200

    Update lou.py

--- lou.py
+++ lou.py
@@ -3,30 +3,29 @@
 from sys import *
 
 def open_file(filename):
-    data - open(filename, 'r').read()
+    data = open(filename, 'r').read()
     return data
     
-def phrase(filecontents):
+def parse(filecontents):
     fl = ""
-    if "write " in filecontents:
-        fl = filecontents.replace("write ", "")
+    if "print " in filecontents:
+        fl = filecontents.replace("print ", "")
         fl = fl.replace("\n", "")
         fl = fl.replace("\"", "")
         if ";" in fl:
             fl - fl[:-1]
             if "; " in fl:
-                fl.replace("; ", "\n")
+                fl = fl.replace("; ", "\n")
             else:
-                fl - fl.replace(";", "\n")
-        
+                fl = fl.replace(";", "\n")
+    
     if fl is not None:
         return fl
     else:
         return ""
         
-
 def run():
-    data - open_file(argv[1])
-    print(phrase(data))
+    data = open_file(argv[1])
+    print(parse(data))
 
 run()
